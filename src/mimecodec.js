895f624ff7d480d762d662ba1c3bb91831dc4646
Björgvin Ragnarsson <nifgraup@gmail.com>
2018-03-01 11:51:22 +0200

    fix regex catastrophic backtracking

--- src/mimecodec.js
+++ src/mimecodec.js
@@ -177,8 +177,8 @@ const qEncodeForbiddenHeaderChars = function (str) {
  * @return {String} String with possible mime words
  */
 export function mimeWordsEncode (data = '', mimeWordEncoding = 'Q', fromCharset = 'UTF-8') {
-  const regex = /([^\s\u0080-\uFFFF]*[\u0080-\uFFFF]+[^\s\u0080-\uFFFF]*(?:\s+[^\s\u0080-\uFFFF]*[\u0080-\uFFFF]+[^\s\u0080-\uFFFF]*\s*)?)+(?=\s|$)/g
-  return decode(convert(data, fromCharset)).replace(regex, match => match.length ? mimeWordEncode(match, mimeWordEncoding, fromCharset) : '')
+  const regex = /[^ \u0080-\uFFFF]*[\u0080-\uFFFF][^ ]*( +[^ \u0080-\uFFFF]*[\u0080-\uFFFF][^ ]*)*/g
+  return decode(convert(data, fromCharset)).replace(regex, match => mimeWordEncode(match, mimeWordEncoding, fromCharset))
 }
 
 /**
