184473978514fb99bec51696b1cd1faf449fef5b
Nicu Chiciuc <nicuchiciuc@gmail.com>
2023-01-13 15:24:13 +0200

    create the type declaration in a separate step

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf $PWD/dist
-npx babel src --extensions ".ts" --out-dir dist --ignore '**/*.unit.ts' --source-maps inline
-git reset
-git add $PWD/dist
-git commit -m 'Updating dist files' -n
